099f85b59eb0228c31fc43f0930247fbbc900eae
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-24 13:35:05 -0700

    google fonts and bootstrap imported

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "./components/Header";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Portfolio from "./components/Portfolio";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
-        <Header />
+        <Portfolio />
       </div>
     </Router>
   )
