762f8bdab6a172f777723882b71e00056e571253
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-31 12:40:46 -0700

    route adjust

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 app.listen(PORT, function() {
     console.log(`Listening on ${PORT}`);
