0ebbc07c063bcf9986c8ffb4b09c9b5efa4d5b05
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-31 23:29:56 -0700

    heroku server fix

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const routes = require("./routes");
 const app = express();
 var index = require('./routes/index');
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
