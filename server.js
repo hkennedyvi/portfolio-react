90a8d87afd8ae5e7f393b692a214826d82896579
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-31 23:23:19 -0700

    route adjustments

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const routes = require("./routes");
 const app = express();
+var index = require('./routes/index');
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
+
+app.use('/', index);
 app.use(routes);
+
 
 app.listen(PORT, function() {
     console.log(`Listening on ${PORT}`);
